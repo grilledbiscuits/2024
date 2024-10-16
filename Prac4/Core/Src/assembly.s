d0c8eab582095b12f052ae313d64335947b6bb25
Jack Youens <YNSJAC001@myuct.ac.za>
2024-10-16 17:30:25 +0200

    Purring like a kitten

--- Prac4/Core/Src/assembly.s
+++ Prac4/Core/Src/assembly.s
@@ -35,7 +35,7 @@ ASM_Main:
 @ TODO: Add code, labels and logic for button checks and LED patterns
 
 main_loop:
-
+	LDR R0, GPIOA_BASE		@ You gotta keep reloading that register bud
 	LDR R4, [R0, #0x10]		@IDR OFFSET VALUE
 
 	@DEFAULT STATE			(LED increments by 1 every 0.7sec)
@@ -46,43 +46,68 @@ main_loop:
 	BEQ incr_normal
 
 	@BUTTON 0 CHECK			(LED increments by 2)
+	MOVS R6, #0b1111
+	ANDS R4, R4, R6
 	CMP R4, #0b1110
+	BEQ incr_twiceandslow
 
 	@BUTTON 1 CHECK			(LED increments by 1 every 0.3sec)
+	MOVS R6, #0b1111
+	ANDS R4, R4, R6
 	CMP R4, #0b1101
+	BEQ incr_fast
 
 	@BUTTON 2 CHECK			(LEDs display 0xAA until released)
 	CMP R4, #0b1011
+	BEQ AlcoholicsAnonymous
 
 	@BUTTON 1 AND 2 CHECK	(Increment by 2 every 0.3sec)
 	CMP R4, #0b1100
+	BEQ incr_twiceandfast
 
 	@BUTTON 3 CHECK	(Freeze)
 	CMP R4, #0b0111
-
+	BEQ freeze
 
 incr_normal:
 	CMP R2, #0b00000000		@(Check if there are any on LEDs to shift)
-	BEQ reset_slow			@(Send to restart (R2=1) block
+	BEQ reset				@(Send to restart (R2=1) block
 	LSLS R2, R2, #1			@Shift left by 1
 	BL longdelay
 	B maskled
 
 incr_fast:
 	CMP R2, #0b00000000		@(Check if there are any on LEDs to shift)
-	BEQ reset_fast			@(Send to restart (R2=1) block
+	BEQ reset				@(Send to restart (R2=1) block
 	LSLS R2, R2, #1			@Shift left by 1
 	BL shortdelay
 	B maskled
 
-reset_fast:
-	MOVS R2, #1
+incr_twiceandslow:
+	CMP R2, #0b00000000		@(Check if there are any on LEDs to shift)
+	BEQ reset				@(Send to restart (R2=1) block
+	LSLS R2, R2, #2			@Shift left by 1
+	BL longdelay
+	B maskled
+
+incr_twiceandfast:
+	CMP R2, #0b00000000		@(Check if there are any on LEDs to shift)
+	BEQ reset				@(Send to restart (R2=1) block
+	LSLS R2, R2, #2			@Shift left by 1
 	BL shortdelay
 	B maskled
 
-reset_slow:
+AlcoholicsAnonymous:
+    MOVS R3, #0xAA			@Write AA to a dummy register
+    LDR R0, GPIOB_BASE		@Update that register baby
+    STR R3, [R0, #0x14] 	@ Write 0xAA to the LED ODR
+    B main_loop
+
+freeze:@!
+	B maskled				@Literally do nothing
+
+reset:
 	MOVS R2, #1
-	BL longdelay
 	B maskled
 
 maskled:
@@ -90,20 +115,18 @@ maskled:
 	ANDS R2, R2, R3
 	B write_leds
 
-
-
 longdelay:
 	LDR R3, LONG_DELAY_CNT
-	loop:
+	looplong:
 		SUBS R3, R3, #1
-		BNE loop
+		BNE looplong
 		BX LR
 
 shortdelay:
 	LDR R3, SHORT_DELAY_CNT
-	loop:
+	loopshort:
 		SUBS R3, R3, #1
-		BNE loop
+		BNE loopshort
 		BX LR
 
 write_leds:
